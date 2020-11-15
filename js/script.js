c169ea66f8f536f00d9ddfb96d3feffc9fdd450a
M.Taha Gouyandeh.B <67068134+Mtaha00@users.noreply.github.com>
2020-11-15 10:46:04 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,185 @@
+var languages = {
+    en:{
+		tahaHS:"Home",
+        hi : "hi",
+        Name : "I'm Mohammad taha gouyandeh barhaghi.",
+        java : "Java developer",
+        sql : "SQL DB ",
+		More: "More About Me",
+		lang: "Select Your language ",
+		About: " About",
+		About1: " About",
+		Let: "Let me introduce myself.",
+		IMM: "im mohammad taha gouyandeh barhaghi simple programmer and I live in iran (tehran).",
+		Profile1: "Profile",
+		Profilee: "i born is 2000 in iran (tehran) and i go to payam nor univercity in tehran.",
+		FName: "Fullname:",
+		FNameL: "Mohammad Taha Gouayndeh Barhaghi",
+		BiDA: "Birth Date",
+		BiDA1:"April 17, 2000",
+		JB:"Job",
+		JB1: "FreeLancer, Java Developer, work in publish",
+		WebS: "Website:",
+		EMIL:"Email:",
+		SKLL:"Skills",
+		SKLL1: "I'm a junior java programmer and SQL DB and I worked with WordPress for a while.",
+		Hiree:"Hire me",
+		ReS:"Resume",
+		ReS2:"Resume",
+   		ReS1:"More of my credentials.",
+		WorkP:"work in publish",
+		WorkPD:"July 2017 - Present",
+		mahdi:"Mahdi press",
+		mahdi1:"I have been working in my father's publishing house since 2017. Here, computer books have been published in English and Persian for about 20 years.",
+		taha1:"java Developer",
+	   	taha2:"September 2019 - present",
+		taha3:"Junior Free lancer",
+		taha4:"I am currently doing small projects as a junior java programmer and I am studying to improve my knowledge and skills.",
+		taha5:"DB Programmer",
+	   	taha6:"June 2020 - present",
+		taha7:"Junior Free lancer",
+	   	taha8:"I am currently doing small projects as a junior SQL programmer and I am studying to improve my knowledge and skills.",
+	   	tahae:"Education",	
+		tahae1:"Bachelor Degree",
+	   	tahae2:"July 2018 - Present",
+		tahae3:"Paym Nur University of Tehran",
+		tahae4:"In 2018, I went to Payamnoor University in Tehran. One of the reasons I went to this university was to study in my hometown and go to my favorite field, which was computer. In the world, this university has a better reputation than some Iranian universities.",		
+		tahaCo:"Contact",
+		tahaCo1:"Contact",
+   		tahaCo1:"I'd Love To Hear From You.",
+		SSub:"Submit",
+		massagesend:"Your message was sent, thank you!",
+		tahahome:"Where to find me",
+		tahaEm:"Email Me At",
+		tahaCa:"Call Me At",
+		tahaCopy:"©Copyright Taha",
+		
+	},
+    fa:{
+		tahaHS:"خانه",
+        hi : "سلام",
+        Name : "من محمد طاها گوینده برحقی هستم",
+        java : "توسعه دهنده جاوا",
+        sql : "sql دیتا بیس",
+		More: "در باره ی من",
+		lang: "زبان خود را انتخاب کنید",
+		About: "درباره من",
+		About1: "درباره من",
+		Let: "اجازه دهید خودم را معرفی کنم",
+		IMM: "من محمد طاها گوینده برحقی هستم یک برنامه نویس تازه کار و من در ایران(تهران) زندگی میکنم",
+		Profile1: "پرفایل",
+		Profilee: "من متولد سال 1379 در ایران(تهران) هستم و در دانشگاه پیامنور استان تهران مشغول به تحصیل می باشم",
+		FName: "نام و نام خانوادگی:",
+		FNameL: "محمد طاها گوینده برحقی",
+		BiDA: "تاریخ تولد:",
+		BiDA1:"29 فروردین 1379",
+		JB:"شغل:",
+		JB1:"فریلنسر توسعه دهنده جاوا و کار در انتشارات",
+		WebS: "وبسایت:",
+		EMIL:"ایمیل:",
+		SKLL:"مهارت ها",
+		SKLL1: "من یک برنامه نویس تازه کار با زبان جاوا و دیتا بیس SQL هستم و مدتی هم با WordPress کار کردم",
+		Hiree:"تماس با من",
+		ReS:"رزومه",
+		ReS2:"رزومه",
+   		ReS1:"تجارب کاری من",
+		WorkP:"کار در انتشارات",
+		WorkPD:"تابستان 97 تا کنون",
+		mahdi:"انتشارات مهدی",
+		mahdi1:"من از سال 97 در انتشارات پدرم کار می کردم. حدود 20 سال است که در اینجا کتاب های رایانه ای به زبان انگلیسی و فارسی منتشر می شود.",
+		taha1:"توسعه دهنده جاوا",
+	   	taha2:"از پاییز 98 تا کنون",
+		taha3:"فریلنسر جاوا",
+		taha4:"در حال حاظر من به عنوان برنامه نویس مبتدی جاوا پروژه های کوچک  انجام میدهم و در حال مطالعه و تمرین برای ارتقای مهارت و اطلاعات خودم هستم",
+		taha5:"توسعه دهنده دیتابیس",
+	   	taha6:"تابستان 99 تا کنون",
+		taha7:"فریلنسر جاوا",
+		taha8:"در حال حاظر من به عنوان توسعه دهنده مبتدی sql  پروژه های کوچک  انجام میدهم و در حال مطالعه و تمرین برای ارتقای مهارت و اطلاعات خودم هستم",
+		tahae:"مدارک ",
+		tahae1:"لیسانس",
+		tahae2:"از 97 تا کنون",
+		tahae3:"پیام نور تهران شمال",
+	   	tahae4:"من از سال 97 به دانشگاه پیامنور استن تهران میروم از دلایل انتخاب این دانشگاه برای من تحصیل در شهر خودم و رشته ی مورد علاقم که کامپیوتر هست و اعتبار دانشگاه بود", 	
+		tahaCo:"ارتباط با من",
+		tahaCo2:"ارتباط با من",
+   		tahaCo1:"من علاقه مندم حرف های شما را بشنوم",
+		SSub:"ارسال",
+		massagesend:"پیام شما ارسال شد متشکرم",
+		tahahome:"محل زندگی من",
+		tahahome1:"ایران تهران",
+		tahaEm:"ایمیل های من",
+		tahaCa:"شماره تماس های من",
+		tahaCopy:"©کپی رایت طاها",
+		
+	}
+}
+
+if(window.location.hash){
+    if(window.location.hash === "#fa"){
+        hi.textContent = languages.fa.hi;
+        Name.textContent = languages.fa.Name;
+        java.textContent = languages.fa.java;
+        sql.textContent = languages.fa.sql;
+		More.textContent = languages.fa.More;
+		lang.textContent = languages.fa.lang;
+		About.textContent = languages.fa.About;
+		About1.textContent = languages.fa.About1;
+		Let.textContent = languages.fa.Let;
+		IMM.textContent = languages.fa.IMM;
+		Profile1.textContent = languages.fa.Profile1;
+		Profilee.textContent = languages.fa.Profilee;
+		FName.textContent = languages.fa.FName;
+		FNameL.textContent = languages.fa.FNameL;
+		BiDA.textContent = languages.fa.BiDA;
+		BiDA1.textContent = languages.fa.BiDA1;
+		JB.textContent = languages.fa.JB;
+		JB1.textContent = languages.fa.JB1;
+		WebS.textContent = languages.fa.WebS;
+		EMIL.textContent = languages.fa.EMIL;
+		SKLL.textContent = languages.fa.SKLL;
+		SKLL1.textContent = languages.fa.SKLL1;
+		Hiree.textContent = languages.fa.Hiree;
+		ReS.textContent = languages.fa.ReS;
+		ReS2.textContent = languages.fa.ReS2;
+		ReS1.textContent = languages.fa.ReS1;
+		WorkP.textContent = languages.fa.WorkP;
+		WorkPD.textContent = languages.fa.WorkPD;
+		mahdi.textContent = languages.fa.mahdi;
+		mahdi1.textContent = languages.fa.mahdi1;
+		taha1.textContent = languages.fa.taha1;
+		taha2.textContent = languages.fa.taha2;
+		taha3.textContent = languages.fa.taha3;
+		taha4.textContent = languages.fa.taha4;
+		taha5.textContent = languages.fa.taha5;
+		taha6.textContent = languages.fa.taha6;
+		taha7.textContent = languages.fa.taha7;
+		taha8.textContent = languages.fa.taha8;
+		tahae.textContent = languages.fa.tahae;
+		tahae1.textContent = languages.fa.tahae1;
+		tahae2.textContent = languages.fa.tahae2;
+		tahae3.textContent = languages.fa.tahae3;
+		tahae4.textContent = languages.fa.tahae4;
+		tahaCo.textContent = languages.fa.tahaCo;
+		tahaCo2.textContent = languages.fa.tahaCo2;
+		tahaCo1.textContent = languages.fa.tahaCo1;
+		SSub.textContent = languages.fa.SSub;
+		massagesend.textContent = languages.fa.massagesend;
+		tahahome.textContent = languages.fa.tahahome;
+		tahahome1.textContent = languages.fa.tahahome1;
+		tahaEm.textContent = languages.fa.tahaEm;
+		tahaCa.textContent = languages.fa.tahaCa;
+		tahaHS.textContent = languages.fa.tahaHS;
+		tahaCopy.textContent = languages.fa.tahaCopy;
+    
+		document.body.style.direction = "rtl";
+    }
+}
+
+var reload = document.querySelectorAll('.reload');
+
+for( i = 0 ; i <= reload.length ; i++){
+    reload[i].onclick = function(){
+        window.location.hash = this.getAttribute('href');
+        window.location.reload();
+    }
+}
